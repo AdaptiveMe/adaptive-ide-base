046d305417288065e3c34353041dd97a67e6af36
Rafael Roman <panthro.rafael@gmail.com>
2015-03-11 16:11:05 +0100

    Throwing a meaningful exception when submodule is not initialized

---
+++ get-idea.sh
@@ -0,0 +1,5 @@
+echo "initializing the submodule"
+git submodule update --init
+echo "submodule initialized, checking out the tag webstorm/140.2574"
+cd intellij-community
+git checkout webstorm/140.2574
