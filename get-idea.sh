9be60a9cc195daa833880dae22fc67d920eda473
Rafael Roman <panthro.rafael@gmail.com>
2015-03-12 17:54:37 +0100

    updating tag to webstorm/139.1112

--- get-idea.sh
+++ get-idea.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 echo "initializing the submodule"
 git submodule update --init
-echo "submodule initialized, checking out the tag webstorm/140.2574"
+echo "submodule initialized, checking out the tag webstorm/139.1112"
 cd intellij-community
-git checkout webstorm/140.2574
+git checkout webstorm/139.1112
